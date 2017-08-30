25939301ce08abd78bdd91789170f7d70453fab8
Syed N Peeran <r.syed.nizamuddin.peeran@gmail.com>
2017-08-30 19:39:45 +0530

    Trying somthing new

---
+++ processRunner/processRunner/IProcessExecutable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace processRunner
+{
+    public delegate void ProcessExecutionCompleteEventHandler();
+
+    public interface IProcessExecutable
+    {
+        event ProcessExecutionCompleteEventHandler ExecutionComplete;
+
+        bool Execute(out string ErrorMessage);
+    }
+
+}
--- processRunner/processRunner/IProcessOutputReturnAble.cs
+++ processRunner/processRunner/IProcessOutputReturnAble.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace processRunner
 {
-    interface process
+    public interface IProcessOutputReturnAble
     {
-        bool execute(out string message);
+        void ReturnOutput();
     }
 }
