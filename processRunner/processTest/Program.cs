b42ec08539d624451a9fdea539744453aad3fc95
Syed N Peeran <r.syed.nizamuddin.peeran@gmail.com>
2017-08-31 20:09:24 +0530

    test

---
+++ processRunner/processTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using processRunner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace processTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnitTest1 test1 = new UnitTest1();
+
+            IParameteriseAble n= test1.TestNumber();
+            n.SetValue((float)4);
+            Console.WriteLine(n.GetValue());
+            n.SetName("Hello");
+            Console.WriteLine(n.GetName());
+
+            IParameteriseAble t = test1.TestText();
+            t.SetValue("hell");
+            Console.WriteLine(t.GetValue());
+            t.SetName("Hello1");
+            Console.WriteLine(t.GetName());
+
+        }
+    }
+}
--- processRunner/processTest/UnitTest1.cs
+++ processRunner/processTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using processRunner;
 
 namespace processTest
 {
@@ -7,8 +8,23 @@ namespace processTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public IParameteriseAble TestNumber()
         {
+            Number n = new Number();
+            return n;
+        }
+        [TestMethod]
+        public IParameteriseAble TestText()
+        {
+            IParameteriseAble n = new Text();
+            return n;
+        }
+
+        [TestMethod]
+        public IParameteriseAble TestArray()
+        {
+            IParameteriseAble n = new processRunner.Array();
+            return n;
         }
     }
 }
